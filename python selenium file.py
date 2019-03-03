b5ab3cba0e75355675492023a0062d000e543478
emaillalkrishna <emaillalkrishna@gmail.com>
2019-03-03 15:09:56 +0530

    Initial commit

---
+++ python selenium file.py
@@ -0,0 +1,31 @@
+# from selenium import webdriver
+# driver = webdriver.Chrome("C:\\Users\\LAL KRISHNA\\Downloads\\QSpiders\\chromedriver.exe")
+# driver.get("file:///C:/Users/LAL%20KRISHNA/Documents/Selenium%20Qspiders%20Files/02-Mar-2019/myfirst.html")
+# driver.maximize_window()
+# driver.implicitly_wait(30)
+# driver.find_element_by_id("123").send_keys("admin")
+# driver.find_element_by_id("456").send_keys("password")
+# driver.find_element_by_id("789").click()
+
+
+# from selenium import webdriver
+# driver = webdriver.Chrome("C:\\Users\\LAL KRISHNA\\Downloads\\QSpiders\\chromedriver.exe")
+# driver.get("https://www.facebook.com/")
+# driver.maximize_window()
+# driver.implicitly_wait(30)
+# driver.find_element_by_id("email").send_keys("9538237524")
+# driver.find_element_by_id("pass").send_keys("ikkarakkunnu")
+# driver.find_element_by_id("loginbutton").click()
+
+# from selenium import webdriver
+# driver = webdriver.Chrome("C:\\Users\\LAL KRISHNA\\Downloads\\QSpiders\\chromedriver.exe")
+# driver.get("https://www.facebook.com/")
+# driver.maximize_window()
+# driver.implicitly_wait(30)
+# # driver.find_element_by_id("email").send_keys("9538237524")
+# # driver.find_element_by_id("pass").send_keys("ikkarakkunnu")
+# # driver.find_element_by_id("loginbutton").click()
+# driver.find_element_by_xpath("//input[@id='u_0_j']").send_keys("Rajnikath")
+# driver.find_element_by_xpath("//input[@name='lastname']").send_keys("Taliva")
+# driver.find_element_by_xpath("//input[@name='reg_email__']").send_keys("9895742412")
+# # driver.find_element_by_xpath()
